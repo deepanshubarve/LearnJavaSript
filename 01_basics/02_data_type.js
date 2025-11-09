78944dccd39d59d519356ab967a0750e025fb6d5
Deepanshu <synosuid69@gmail.com>
2025-11-09 12:00:58 +0530

    file complete

---
+++ 01_basics/02_data_type.js
@@ -0,0 +1,20 @@
+"use Stirct"; // treat all the code as newer version of javascript
+
+//alert (we as using node js not browser)
+
+//number -> 2 to power 53
+//bigint -> more than 2 to power 53
+//string 
+//boolean -> true, false
+//null -> empty value
+//undefined -> value not assigned
+//symbol -> unique identifier
+//object -> key value pair
+
+console.log(typeof true);
+console.log(typeof 123n);
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
