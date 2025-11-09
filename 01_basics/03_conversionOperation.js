32831e46f02bc5fdda3c039872e77fc53f5aa6ed
Deepanshu <synosuid69@gmail.com>
2025-11-09 12:43:11 +0530

    03_complete

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,70 @@
+let score = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let someNumber = Number(score)
+// console.log(someNumber);
+// console.log(typeof someNumber);
+
+// console.log(typeof score);
+
+let valueNumber  = Number(score);
+// console.log(valueNumber);
+// console.log(typeof valueNumber);
+
+// "33" -> 33
+// "33abc" -> NaN
+// true = 1, false = 0
+// ""-> false
+
+let name = "Deep"
+let boolName = Boolean(name)
+//console.log(name);
+// console.log(typeof boolName);
+// console.log(boolName);
+
+/************************************** Operation ****************************/
+
+// console.log(1+1);
+// console.log(4*2);
+// console.log(1/1);
+// console.log(1-1);
+// console.log(4**2);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1"+"2");
+console.log(1 + 2 + "2");//left to right  32
+console.log("1" + 2 +2); // left to right 122
+
+console.log(+true); //covert in interger due to +
+//String concatition
+
+let str1 = "Depanshu";
+let str2  = "barve"
+let str3 = str1 + str2
+console.log(str3);
+
+//Pre and post fix
+
+let game = 100;
+console.log(game++);
+console.log(++game);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
