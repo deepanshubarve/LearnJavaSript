6aa4b7f717a31768a7ebcfce379b3619c46f829a
Deepanshu <synosuid69@gmail.com>
2025-11-09 13:13:48 +0530

    04 complete

---
+++ 01_basics/04_compersion.js
@@ -0,0 +1,35 @@
+let a = 23;
+let b = 44;
+
+
+a > b;
+a == b;
+a <= b;
+console.log(a < b);
+
+//String and number
+
+// console.log("2" > 1);
+// console.log(2 > "1");
+
+// console.log(null > 0); // false
+// console.log(null == 0); //false
+// console.log(null >= 0); // true becoz greater and equal opertor work differently it convert null to number
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+//Strict Check(check datatype also)s
+
+console.log("2" === 2);
+
+
+
+
+
+
+
+
+
+
