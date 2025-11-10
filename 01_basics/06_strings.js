9657b1585bfbad508d04de2a8985a25577517749
Deepanshu <synosuid69@gmail.com>
2025-11-10 21:49:07 +0530

    strings and math-num

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,36 @@
+const name = "hitesh"
+const count = 0;
+
+//String interpolation 
+console.log(`Hello my name is deepanshus and my cgpa is ${7.5}`);
+
+const getname = new String("hitesh")
+
+console.log(getname[0]);
+console.log(getname.__proto__);
+
+console.log(getname.length);
+console.log(getname.charAt(3));
+
+console.log(getname.substring(0,3));
+console.log(getname.slice(-8,1));
+
+const url = `  http-hitesh-bohol   `
+console.log(url.split('-'));
+
+console.log(url);
+console.log(url.trim());
+
+console.log(url.includes("hitesh"));
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01_basics/07_math_num.js
@@ -0,0 +1,9 @@
+
+
+//Mene yha pr string interpolation perform kia hai 
+let age = 14;
+console.log(`my name is dibbu and my age is ${age}`);
+
+
+
+
