8b27b759fcd262ec9ee91bb0478951e7e7062d6b
Deepanshu <synosuid69@gmail.com>
2025-11-11 16:33:03 +0530

    07 complete

--- 01_basics/07_math_num.js
+++ 01_basics/07_math_num.js
@@ -1,8 +1,41 @@
+// const score = 10;
+// console.log(score);
+
+// const nm = new Number(100);
+// console.log(nm);
+
+// console.log(nm.toString.length);
+// console.log(nm.toFixed(2));
+
+// const otherNum = 150.343434
+// console.log(otherNum.toPrecision(2));
+
+// /******************Math********************/
+
+// console.log(Math);
+
+// console.log(Math.abs(-56));
+// console.log(Math.round(67.9909877))
+// console.log(Math.ceil(4.4));
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1 );
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
 
 
-//Mene yha pr string interpolation perform kia hai 
-let age = 14;
-console.log(`my name is dibbu and my age is ${age}`);
+
+
+
+
+
+
+
+
 
 
 
